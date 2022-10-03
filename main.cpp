c3ac3fac95d274ec6b1143bd0246813251cd184d
Sadocha191 <wiktor09112@wp.pl>
2022-10-03 11:42:26 +0200

    second branch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+int count(int a, int b)  {
+    return a+b
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << count(4,2) << "Hello, World!" << std::endl;
     return 0;
 }
